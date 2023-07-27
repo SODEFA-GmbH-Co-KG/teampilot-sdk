7b023fec03b33dc71031076f39b5e8c6808063f8
Tristan Rechenberger <rechenberger@me.com>
2023-07-27 15:51:30 +0200

    🎉

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] last:text-white ">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+          {/* <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
             Teampilot <span className="text-[hsl(280,100%,70%)]">AI</span> SDK
-          </h1>
+          </h1> */}
           <PersonSearch />
         </div>
       </main>
