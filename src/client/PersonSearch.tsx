766a12596b5e99c0d18d9c43519af3c06d9e7c82
Tristan Rechenberger <rechenberger@me.com>
2023-07-27 18:01:16 +0200

    🎉

--- src/client/PersonSearch.tsx
+++ src/client/PersonSearch.tsx
@@ -35,17 +35,20 @@ export const PersonSearch = () => {
       <button onClick={() => refetch()}>Refetch</button>
       {isFetching && <div>Loading...</div>}
       {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
-      {persons?.map((p, idx) => {
-        return (
-          <div key={idx}>
-            <div>{p.firstName}</div>
-            <div>{p.lastName}</div>
-            <div>{p.dateOfBirth}</div>
-            <div>{p.shortDescription}</div>
-            {/* <img src={p.imageUrl} alt="" /> */}
-          </div>
-        )
-      })}
+      <div className="grid grid-cols-3 gap-2">
+        {persons?.map((p, idx) => {
+          return (
+            <div key={idx}>
+              <div className="text-xl">
+                <span>{p.firstName}</span> <strong>{p.lastName}</strong>
+              </div>
+              <div className="font-mono">{p.dateOfBirth}</div>
+              <div className="text-sm opacity-80">{p.shortDescription}</div>
+              {/* <img src={p.imageUrl} alt="" /> */}
+            </div>
+          )
+        })}
+      </div>
     </>
   )
 }
