31783c298104b638e01cecbfcb68fbaf2490b60d
Tristan Rechenberger <rechenberger@me.com>
2023-07-28 16:36:51 +0200

    🎉

--- src/client/Reasons.tsx
+++ src/client/Reasons.tsx
@@ -2,25 +2,6 @@ import { use } from "react"
 import { z } from "zod"
 import { fetchTeampilotData } from "~/teampilot-sdk/teampilot"
 
-export const reasonsCode = `export const Reasons = () => {
-  const reasons = use(
-    fetchTeampilotData({
-      message: "5 Reasons why NextJS is awesome",
-      schema: z.array(z.string()),
-    })
-  )
-  return (
-    <>
-      <ul className="list-inside list-disc">
-        {reasons.map((reason, idx) => {
-          return <li key={idx}>{reason}</li>
-        })}
-      </ul>
-    </>
-  )
-}
-`
-
 export const Reasons = () => {
   const reasons = use(
     fetchTeampilotData({
--- src/client/ReasonsShowCase.tsx
+++ src/client/ReasonsShowCase.tsx
@@ -1,13 +1,17 @@
+import { readFile } from "fs/promises"
+import { use } from "react"
 import { Card, CardContent } from "~/shadcn/components/ui/card"
 import { CodeBlock } from "./CodeBlock"
-import { Reasons, reasonsCode } from "./Reasons"
+import { Reasons } from "./Reasons"
 
 export const ReasonsShowCase = () => {
+  const file = use(readFile("./src/client/Reasons.tsx", { encoding: "utf-8" }))
+
   return (
     <>
       <div className="flex flex-row gap-4">
         <div className="flex-1">
-          <CodeBlock language="tsx" value={reasonsCode} lightMode="dark" />
+          <CodeBlock language="tsx" value={file} lightMode="dark" />
         </div>
         <Card className="flex flex-1 items-center bg-accent p-2">
           <CardContent className="">
