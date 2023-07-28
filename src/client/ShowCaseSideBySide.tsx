ebf2e6643cdc6f4ade0afa1d18ca4c71cb8483ac
Tristan Rechenberger <rechenberger@me.com>
2023-07-28 17:23:55 +0200

    🎉

--- src/client/ShowCaseSideBySide.tsx
+++ src/client/ShowCaseSideBySide.tsx
@@ -1,3 +1,4 @@
+import { ArrowRight } from "lucide-react"
 import { type ReactNode } from "react"
 import { Card, CardContent } from "~/shadcn/components/ui/card"
 import { CodeBlock } from "./CodeBlock"
@@ -11,10 +12,11 @@ export const ShowCaseSideBySide = ({
 }) => {
   return (
     <>
-      <div className="flex flex-row gap-8">
+      <div className="flex flex-row items-center gap-8">
         <div className="flex-1">
           <CodeBlock language="tsx" value={code} lightMode="dark" />
         </div>
+        <ArrowRight className="h-4 w-4" />
         <Card className="flex flex-1 items-center bg-accent p-2">
           <CardContent>{children}</CardContent>
         </Card>
