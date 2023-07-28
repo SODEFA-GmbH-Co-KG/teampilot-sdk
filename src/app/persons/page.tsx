de620332d1729b5083e38acf4e3cf3b0d31ca3b0
Tristan Rechenberger <rechenberger@me.com>
2023-07-28 16:49:02 +0200

    🎉

--- src/app/persons/page.tsx
+++ src/app/persons/page.tsx
@@ -1,61 +1,12 @@
-import { use } from "react"
-import { z } from "zod"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "~/shadcn/components/ui/card"
-import { fetchTeampilotData } from "~/teampilot-sdk/teampilot"
+import { Persons } from "~/client/Persons"
+import { ShowCaseSideBySide } from "~/client/ShowCaseSideBySide"
 
 export default function Page() {
-  const data = use(
-    fetchTeampilotData({
-      message: "First 6 Presidents of the US",
-      schema: z.array(
-        z.object({
-          firstName: z.string(),
-          lastName: z.string(),
-          dateOfBirth: z.string(),
-          shortDescription: z.string(),
-          // noOfPresident: z.number(),
-          achievements: z.array(z.string()),
-        })
-      ),
-    })
-  )
-
   return (
     <>
-      <div>Persons</div>
-      <div className="grid grid-cols-3 gap-4">
-        {data?.map((p, idx) => {
-          return (
-            <Card key={idx}>
-              <CardHeader>
-                <CardTitle>
-                  {/* <span className="text-muted-foreground">
-                    #{p.noOfPresident}
-                  </span>{" "} */}
-                  <span>{p.firstName}</span> <strong>{p.lastName}</strong>
-                </CardTitle>
-                <div className="font-mono">{p.dateOfBirth}</div>
-              </CardHeader>
-              <CardContent>
-                <CardDescription>
-                  <div>{p.shortDescription}</div>
-                  <ul className="mt-2 list-inside list-disc">
-                    {p.achievements.map((a, idx) => {
-                      return <li key={idx}>{a}</li>
-                    })}
-                  </ul>
-                </CardDescription>
-              </CardContent>
-            </Card>
-          )
-        })}
-      </div>
+      <ShowCaseSideBySide file="./src/client/Persons.tsx">
+        <Persons />
+      </ShowCaseSideBySide>
     </>
   )
 }
