e661e4580f37cba01663255103f2be0afaef5924
Tristan Rechenberger <rechenberger@me.com>
2023-07-28 13:59:26 +0200

    🎉

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import { type ReactNode } from "react"
 import { ThemeProvider } from "~/shadcn/components/theme-provider"
+import "~/styles/globals.css"
 
 export default function RootLayout({ children }: { children?: ReactNode }) {
   return (
     <>
       <html lang="en" suppressHydrationWarning>
         <head />
-        <body>
+        <body className="min-h-screen bg-background font-sans antialiased">
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             {children}
           </ThemeProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { DarkModeToggle } from "~/client/DarkModeToggle"
+import { Button } from "~/shadcn/components/ui/button"
+
 export default function Page() {
   return (
     <>
-      <div>hi</div>
+      <h1>hi</h1>
+      <Button>Button</Button>
+      <DarkModeToggle />
     </>
   )
 }
