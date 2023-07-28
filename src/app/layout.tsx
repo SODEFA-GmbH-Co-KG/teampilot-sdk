4be45945ea042294dc59346f3fc12cfc1a1bdbf4
Tristan Rechenberger <rechenberger@me.com>
2023-07-28 15:02:31 +0200

    🎉

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { type ReactNode } from "react"
+import { TeampilotLogo } from "~/client/TeampilotLogo"
 import { ThemeProvider } from "~/shadcn/components/theme-provider"
 import "~/styles/globals.css"
 
@@ -6,10 +7,25 @@ export default function RootLayout({ children }: { children?: ReactNode }) {
   return (
     <>
       <html lang="en" suppressHydrationWarning>
-        <head />
+        <head>
+          <link rel="icon" href="/favicon.svg" />
+        </head>
         <body className="min-h-screen bg-background font-sans antialiased">
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            {children}
+            <>
+              <div className="container flex flex-col gap-8 py-8">
+                <div className="flex flex-row gap-2">
+                  <TeampilotLogo className="h-8 w-8" />
+                  <div className="text-xl">
+                    <strong>Teampilot SDK</strong>
+                  </div>
+                </div>
+              </div>
+              <hr />
+              <div className="container flex flex-col gap-8 py-8">
+                {children}
+              </div>
+            </>
           </ThemeProvider>
         </body>
       </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { Button } from "~/shadcn/components/ui/button"
 export default function Page() {
   return (
     <>
-      <h1>hi</h1>
       <Button>Button</Button>
       <DarkModeToggle />
     </>
