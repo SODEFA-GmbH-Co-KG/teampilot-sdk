dc1c6ad66ac5069a1d17ce8ac083c0694c411cba
Tristan Rechenberger <rechenberger@me.com>
2023-07-28 15:17:00 +0200

    🎉

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Space_Grotesk } from "next/font/google"
 import { type ReactNode } from "react"
 import { DarkModeToggle } from "~/client/DarkModeToggle"
+import { MainTopNav } from "~/client/MainTopNav"
 import { TeampilotLogo } from "~/client/TeampilotLogo"
 import { ThemeProvider } from "~/shadcn/components/theme-provider"
 import { cn } from "~/shadcn/utils"
@@ -25,13 +26,14 @@ export default function RootLayout({ children }: { children?: ReactNode }) {
         >
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <>
-              <div className="container flex flex-row items-center gap-4 py-6">
+              <div className="container flex flex-row items-center gap-6 py-6">
                 <div className="flex flex-row items-center gap-3">
                   <TeampilotLogo className="h-8 w-8" />
                   <div className="text-xl">
                     <strong>Teampilot SDK</strong>
                   </div>
                 </div>
+                <MainTopNav />
                 <div className="flex-1" />
                 <DarkModeToggle />
               </div>
---
+++ src/client/MainTopNav.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { cn } from "~/shadcn/utils"
+
+const entries = [
+  {
+    name: "Overview",
+    href: "/",
+  },
+  {
+    name: "Persons",
+    href: "/persons",
+  },
+]
+
+export function MainTopNav({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLElement>) {
+  const pathname = usePathname()
+
+  return (
+    <nav
+      className={cn("flex items-center space-x-4 lg:space-x-6", className)}
+      {...props}
+    >
+      {entries.map((entry) => {
+        const isActive =
+          entry.href === "/"
+            ? pathname === entry.href
+            : pathname?.startsWith(entry.href)
+        return (
+          <Link
+            key={entry.href}
+            href={entry.href}
+            className={cn(
+              "text-sm font-medium transition-colors hover:text-primary",
+              !isActive && "text-muted-foreground"
+            )}
+          >
+            {entry.name}
+          </Link>
+        )
+      })}
+    </nav>
+  )
+}
