52336a38717a79a9b4e74f0487c3f16fcaf0944f
Tristan Rechenberger <rechenberger@me.com>
2023-07-28 15:04:58 +0200

    🎉

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { type ReactNode } from "react"
+import { DarkModeToggle } from "~/client/DarkModeToggle"
 import { TeampilotLogo } from "~/client/TeampilotLogo"
 import { ThemeProvider } from "~/shadcn/components/theme-provider"
 import "~/styles/globals.css"
@@ -13,13 +14,15 @@ export default function RootLayout({ children }: { children?: ReactNode }) {
         <body className="min-h-screen bg-background font-sans antialiased">
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <>
-              <div className="container flex flex-col gap-8 py-8">
-                <div className="flex flex-row gap-2">
+              <div className="container flex flex-row items-center gap-4 py-6">
+                <div className="flex flex-row items-center gap-3">
                   <TeampilotLogo className="h-8 w-8" />
                   <div className="text-xl">
                     <strong>Teampilot SDK</strong>
                   </div>
                 </div>
+                <div className="flex-1" />
+                <DarkModeToggle />
               </div>
               <hr />
               <div className="container flex flex-col gap-8 py-8">
