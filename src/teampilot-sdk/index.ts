491b5dd5cc3392de47b85ad8cd74f9c5726ef1e0
Tristan Rechenberger <rechenberger@me.com>
2023-07-29 11:21:13 +0200

    🎉

--- src/teampilot-sdk/index.ts
+++ src/teampilot-sdk/index.ts
@@ -2,13 +2,16 @@ import { z } from "zod"
 import { env } from "~/env.mjs"
 import { transformZodSchemaToOpenAi } from "./transformFunctionToOpenAi"
 
+type RequestOptions = Omit<RequestInit, "body" | "method">
+
 export const fetchTeampilot = async <T extends z.Schema = z.ZodUndefined>({
   message,
   schema,
+  ...requestOptions
 }: {
   message: string
   schema?: T
-}) => {
+} & RequestOptions) => {
   // const url = `http://localhost:3000/api/rest/message`
   const url = `https://teampilot.ai/api/rest/message`
 
@@ -16,12 +19,14 @@ export const fetchTeampilot = async <T extends z.Schema = z.ZodUndefined>({
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      ...requestOptions.headers,
     },
     body: JSON.stringify({
       launchpadSlugId: env.NEXT_PUBLIC_LAUNCHPAD_SLUG_ID,
       message,
       schema: schema ? transformZodSchemaToOpenAi(schema) : null,
     }),
+    ...requestOptions,
   })
 
   const responseSchema = z.object({
@@ -43,15 +48,18 @@ export const fetchTeampilot = async <T extends z.Schema = z.ZodUndefined>({
 export const fetchTeampilotData = async <T extends z.Schema>({
   message,
   schema,
+
+  ...requestOptions
 }: {
   message: string
   schema: T
-}) => {
+} & RequestOptions) => {
   const response = await fetchTeampilot({
     message,
     schema: z.object({
       response: schema,
     }),
+    ...requestOptions,
   })
 
   const data = response.message?.data?.response
