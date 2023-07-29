438d64ceef284c00da0c96f0dac080904d1c7ad6
Tristan Rechenberger <rechenberger@me.com>
2023-07-29 10:47:08 +0200

    🎉

--- src/client/Persons.tsx
+++ src/client/Persons.tsx
@@ -1,4 +1,3 @@
-import { use } from "react"
 import { z } from "zod"
 import {
   Card,
@@ -9,52 +8,42 @@ import {
 } from "~/shadcn/components/ui/card"
 import { fetchTeampilotData } from "~/teampilot-sdk"
 
-export const Persons = () => {
-  const data = use(
-    fetchTeampilotData({
-      message: "First 6 Presidents of the US",
-      schema: z.array(
-        z.object({
-          firstName: z.string(),
-          lastName: z.string(),
-          dateOfBirth: z.string(),
-          shortDescription: z.string(),
-          // noOfPresident: z.number(),
-          achievements: z.array(z.string()),
-        })
-      ),
-    })
-  )
+export const Persons = async () => {
+  const data = await fetchTeampilotData({
+    message: "First 6 Presidents of the US",
+    schema: z.array(
+      z.object({
+        firstName: z.string(),
+        lastName: z.string(),
+        dateOfBirth: z.string(),
+        shortDescription: z.string(),
+        achievements: z.array(z.string()),
+      })
+    ),
+  })
 
   return (
-    <>
-      <div className="grid grid-cols-3 gap-4">
-        {data?.map((p, idx) => {
-          return (
-            <Card key={idx}>
-              <CardHeader>
-                <CardTitle>
-                  {/* <span className="text-muted-foreground">
-                    #{p.noOfPresident}
-                  </span>{" "} */}
-                  <span>{p.firstName}</span> <strong>{p.lastName}</strong>
-                </CardTitle>
-                <div className="font-mono">{p.dateOfBirth}</div>
-              </CardHeader>
-              <CardContent>
-                <CardDescription>
-                  <div>{p.shortDescription}</div>
-                  <ul className="mt-2 list-inside list-disc">
-                    {p.achievements.map((a, idx) => {
-                      return <li key={idx}>{a}</li>
-                    })}
-                  </ul>
-                </CardDescription>
-              </CardContent>
-            </Card>
-          )
-        })}
-      </div>
-    </>
+    <div className="grid grid-cols-3 gap-4">
+      {data?.map((p, idx) => (
+        <Card key={idx}>
+          <CardHeader>
+            <CardTitle>
+              <span>{p.firstName}</span> <strong>{p.lastName}</strong>
+            </CardTitle>
+            <div className="font-mono">{p.dateOfBirth}</div>
+          </CardHeader>
+          <CardContent>
+            <CardDescription>
+              <div>{p.shortDescription}</div>
+              <ul className="mt-2 list-inside list-disc">
+                {p.achievements.map((a, idx) => {
+                  return <li key={idx}>{a}</li>
+                })}
+              </ul>
+            </CardDescription>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
   )
 }
