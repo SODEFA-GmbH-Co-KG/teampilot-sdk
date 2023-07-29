e6107c417cd0a106cb0caeeea596d10e5fb8d852
Tristan Rechenberger <rechenberger@me.com>
2023-07-29 23:22:35 +0200

    🎉

--- src/app/dev/page.tsx
+++ src/app/dev/page.tsx
@@ -1,20 +1,19 @@
-import { use } from "react"
 import { CodeBlock } from "~/client/CodeBlock"
 import { fetchTeampilot } from "~/teampilot-sdk"
 
-export default function Page() {
-  const answer = use(
-    fetchTeampilot({
-      url: `http://localhost:3000/api/rest/message`,
-      launchpadSlugId: process.env.NEXT_PUBLIC_LAUNCHPAD_SLUG_ID_LOCAL,
-      message: "Draw an image of a sunset",
-      // schema: z.object({
-      //   hours: z.string(),
-      //   minutes: z.string(),
-      // }),
-      cache: "no-cache",
-    })
-  )
+export default async function Page() {
+  const launchpadSlugId = process.env.NEXT_PUBLIC_LAUNCHPAD_SLUG_ID
+  if (!launchpadSlugId) return null
+  const answer = await fetchTeampilot({
+    url: `http://localhost:3000/api/rest/message`,
+    launchpadSlugId,
+    message: "Draw an image of a sunset",
+    // schema: z.object({
+    //   hours: z.string(),
+    //   minutes: z.string(),
+    // }),
+  })
+
   return (
     <>
       <CodeBlock
