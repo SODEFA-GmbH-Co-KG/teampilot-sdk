d6a8874ae88f3b2815b2ce532b2d34313fabc802
Tristan Rechenberger <rechenberger@me.com>
2023-07-29 11:15:29 +0200

    🎉

--- src/client/ShowCaseSideBySide.tsx
+++ src/client/ShowCaseSideBySide.tsx
@@ -1,14 +1,18 @@
 import { ArrowRight } from "lucide-react"
 import { type ReactNode } from "react"
 import { Card, CardContent } from "~/shadcn/components/ui/card"
+import { cn } from "~/shadcn/utils"
 import { CodeBlock } from "./CodeBlock"
+import { SuspenseLoader } from "./SuspenseLoader"
 
 export const ShowCaseSideBySide = ({
   code,
   children,
+  align = "center",
 }: {
   code: string
   children?: ReactNode
+  align?: "center" | "start" | "end"
 }) => {
   return (
     <>
@@ -17,8 +21,20 @@ export const ShowCaseSideBySide = ({
           <CodeBlock language="tsx" value={code} lightMode="dark" />
         </div>
         <ArrowRight className="h-8 w-8 self-center" />
-        <Card className="flex flex-1 items-center bg-accent p-4">
-          <CardContent>{children}</CardContent>
+        <Card
+          className={cn(
+            "flex flex-1 flex-col justify-center bg-accent p-4",
+
+            {
+              "items-center": align === "center",
+              "items-start": align === "start",
+              "items-end": align === "end",
+            }
+          )}
+        >
+          <CardContent>
+            <SuspenseLoader>{children}</SuspenseLoader>
+          </CardContent>
         </Card>
       </div>
     </>
