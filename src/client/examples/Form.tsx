5991ddab1cb0e8560ca07de0f39ce9422b63bb60
Tristan Rechenberger <rechenberger@me.com>
2023-07-29 19:01:52 +0200

    🎉

--- src/client/examples/Form.tsx
+++ src/client/examples/Form.tsx
@@ -1,3 +1,4 @@
+import { Send } from "lucide-react"
 import { Button } from "~/shadcn/components/ui/button"
 import { Input } from "~/shadcn/components/ui/input"
 import { teampilot } from "~/teampilot"
@@ -12,10 +13,12 @@ export const Form = () => {
   }
   return (
     <>
-      <form className="flex flex-col gap-2" action={submit}>
-        <div className="text-center font-bold">Register</div>
+      <div className="mb-2 text-center font-bold">Register</div>
+      <form className="flex flex-row items-center gap-1" action={submit}>
         <Input type="email" placeholder="Your Email" name="email" />
-        <Button type="submit">Submit</Button>
+        <Button type="submit" size="sm">
+          <Send />
+        </Button>
       </form>
     </>
   )
