da1e317b9e87c28cc501561c9593d34b10de8e6b
Tristan Rechenberger <rechenberger@me.com>
2023-07-29 22:47:37 +0200

    🎉

--- src/client/examples/AudioExample.tsx
+++ src/client/examples/AudioExample.tsx
@@ -1,13 +1,12 @@
-import { use } from "react"
 import { teampilot } from "~/teampilot"
 
-export const AudioExample = () => {
-  const media = use(
-    teampilot.functions.fetchMedia({
-      // message: "Read this Website to me: https://teampilot.ai",
-      message: `Read out this document: clkog80n9002mjg08l5idr0cf`,
-    })
-  )
+export const AudioExample = async () => {
+  // const media = await teampilot.websiteToVoice.fetchMedia({
+  //   message: "Read this Website to me: https://teampilot.ai",
+  // })
+  const media = await teampilot.functions.fetchMedia({
+    message: `Read out this document: clkog80n9002mjg08l5idr0cf`,
+  })
 
   return (
     <>
