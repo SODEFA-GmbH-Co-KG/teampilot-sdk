18bc60dff4cfe55c3dce3ef764c6b89ca1cd1b0a
Tristan Rechenberger <rechenberger@me.com>
2023-07-31 13:49:36 +0200

    ✨ todo app

--- src/client/examples/TodoApp/TodoApp.tsx
+++ src/client/examples/TodoApp/TodoApp.tsx
@@ -1,3 +1,31 @@
 export const TodoApp = () => {
-  return <></>
+  // const abc = use(
+  //   teampilot.todoApp.fetchData({
+  //     message: "Get all todos",
+  //     schema: z.array(
+  //       z.object({
+  //         id: z.string(),
+  //         text: z.string(),
+  //         status: z.enum(["completed", "todo", "rescheduled"]),
+  //       })
+  //     ),
+  //   })
+  // )
+
+  // const seed = async () => {
+  //   "use server"
+  //   console.log("seed")
+  //   await teampilot.todoApp.mutate(
+  //     `Create 10 Example Todos. With random title and status`
+  //   )
+  //   console.log("seed done")
+  // }
+
+  return (
+    <>
+      {/* <form action={seed}>
+        <Button type="submit">Seed</Button>
+      </form> */}
+    </>
+  )
 }
