d5121cfdb71c7027ae294a121168c7422927e345
Tristan Rechenberger <rechenberger@me.com>
2023-08-02 12:55:06 +0200

    ♻️ dont use zod during runtime

--- packages/sdk/src/transformFunctionToOpenAi.ts
+++ packages/sdk/src/transformFunctionToOpenAi.ts
@@ -1,7 +1,7 @@
-import { z } from "zod"
+import { type z } from 'zod'
 
 export type OpenAIFunctionParameters = {
-  type: "object" | "array" | "string" | "undefined" | "number" | "null"
+  type: 'object' | 'array' | 'string' | 'undefined' | 'number' | 'null'
   description?: string
   enum?: string[]
   properties?: Record<string, OpenAIFunctionParameters>
@@ -15,10 +15,40 @@ export type OpenAIFunctionDefinition = {
   parameters: OpenAIFunctionParameters
 }
 
+const getZodType = (schema: z.Schema<unknown>) => {
+  return (schema._def as any).typeName as string
+}
+
+const isZodObject = (schema: z.Schema<unknown>): schema is z.ZodObject<any> => {
+  return getZodType(schema) === 'ZodObject'
+}
+const isZodString = (schema: z.Schema<unknown>): schema is z.ZodString => {
+  return getZodType(schema) === 'ZodString'
+}
+const isZodNumber = (schema: z.Schema<unknown>): schema is z.ZodNumber => {
+  return getZodType(schema) === 'ZodNumber'
+}
+const isZodEnum = (schema: z.Schema<unknown>): schema is z.ZodEnum<any> => {
+  return getZodType(schema) === 'ZodEnum'
+}
+const isZodArray = (schema: z.Schema<unknown>): schema is z.ZodArray<any> => {
+  return getZodType(schema) === 'ZodArray'
+}
+const isZodOptional = (
+  schema: z.Schema<unknown>
+): schema is z.ZodOptional<any> => {
+  return getZodType(schema) === 'ZodOptional'
+}
+const isZodDefault = (
+  schema: z.Schema<unknown>
+): schema is z.ZodDefault<any> => {
+  return getZodType(schema) === 'ZodDefault'
+}
+
 export function transformZodSchemaToOpenAi(
   schema: z.Schema<unknown>
 ): OpenAIFunctionParameters {
-  if (schema instanceof z.ZodObject) {
+  if (isZodObject(schema)) {
     const properties: Record<string, OpenAIFunctionParameters> = {}
     const required: string[] = []
 
@@ -35,42 +65,39 @@ export function transformZodSchemaToOpenAi(
     }
 
     return {
-      type: "object",
+      type: 'object',
       properties,
       required,
       description: schema.description,
     }
-  } else if (schema instanceof z.ZodEnum) {
+  } else if (isZodEnum(schema)) {
     return {
-      type: "string",
+      type: 'string',
       enum: schema._def.values,
       description: schema.description,
     }
-  } else if (schema instanceof z.ZodString) {
+  } else if (isZodString(schema)) {
     return {
-      type: "string",
+      type: 'string',
       description: schema.description,
     }
-  } else if (schema instanceof z.ZodNumber) {
+  } else if (isZodNumber(schema)) {
     return {
-      type: "number",
+      type: 'number',
       description: schema.description,
     }
-  } else if (
-    schema instanceof z.ZodDefault ||
-    schema instanceof z.ZodOptional
-  ) {
+  } else if (isZodDefault(schema) || isZodOptional(schema)) {
     const subProperty = transformZodSchemaToOpenAi(schema._def.innerType)
     return {
       ...subProperty,
     }
   }
   //extended with teampilot: https://teampilot.ai/team/sodefa/chat/cljwvoee1000jmf08aelqaum0
-  else if (schema instanceof z.ZodArray) {
+  else if (isZodArray(schema)) {
     const elementType = schema._def.type
     const subProperty = transformZodSchemaToOpenAi(elementType)
     return {
-      type: "array",
+      type: 'array',
       items: subProperty,
       description: schema.description,
     }
