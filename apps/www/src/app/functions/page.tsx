b4a462869a8e0d6dea6e3f2c1e77f0b36d31e9b1
Tristan Rechenberger <rechenberger@me.com>
2023-08-03 11:57:36 +0200

    🐛 showcase form file not found

--- apps/www/src/app/functions/page.tsx
+++ apps/www/src/app/functions/page.tsx
@@ -1,3 +1,4 @@
+import { Form } from "~/client/examples/Form"
 import { Time } from "~/client/examples/Time"
 import { ShowCase } from "~/client/ShowCase"
 
@@ -11,13 +12,39 @@ export default function Page() {
       >
         <Time />
       </ShowCase>
-      {/* <ShowCase
+      <ShowCase
         title="Form"
-        file="/src/client/examples/Form.tsx"
+        code={`import { Send } from "lucide-react"
+import { Button } from "~/shadcn/components/ui/button"
+import { Input } from "~/shadcn/components/ui/input"
+import { teampilot } from "~/teampilot"
+
+export const Form = () => {
+  const submit = async (data: FormData) => {
+    "use server"
+    const email = data.get("email")
+    await teampilot.functions.fetch({
+      message: \`Notify me via discord that a new user has signed up with email: \${email}\`,
+      cacheTtlSeconds: 0,
+    })
+  }
+  return (
+    <>
+      <div className="mb-2 text-center font-bold">Register</div>
+      <form className="flex flex-row items-center gap-1" action={submit}>
+        <Input type="email" placeholder="Your Email" name="email" />
+        <Button type="submit" size="sm">
+          <Send />
+        </Button>
+      </form>
+    </>
+  )
+}
+`}
         layout="side-by-side"
       >
         <Form />
-      </ShowCase> */}
+      </ShowCase>
     </>
   )
 }
--- apps/www/src/client/ShowCase.tsx
+++ apps/www/src/client/ShowCase.tsx
@@ -1,28 +1,30 @@
 import { readFile } from "fs/promises"
 import path from "path"
-import { use, type ReactNode } from "react"
+import { type ReactNode } from "react"
 import { Heading } from "./Heading"
 import { ShowCaseDescription } from "./ShowCaseDescription"
 import { ShowCaseSideBySide } from "./ShowCaseSideBySide"
 import { ShowCaseTabs } from "./ShowCaseTabs"
 import { SuspenseLoader } from "./SuspenseLoader"
 
-export const ShowCase = ({
+export const ShowCase = async ({
+  code = "",
   file,
   children,
   align = "center",
   title,
   layout = "tabs",
 }: {
-  file: string
+  code?: string
+  file?: string
   children?: ReactNode
   align?: "center" | "start" | "end"
   title?: string
   layout?: "tabs" | "side-by-side"
 }) => {
-  const code = use(
-    readFile(path.join(process.cwd(), file), { encoding: "utf-8" })
-  )
+  if (!code && file) {
+    code = await readFile(path.join(process.cwd(), file), { encoding: "utf-8" })
+  }
 
   return (
     <>
