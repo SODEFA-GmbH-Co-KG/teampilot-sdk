3b710457b06b8c1e5472869626b80c3b74c75ef8
Tristan Rechenberger <rechenberger@me.com>
2023-08-03 12:36:40 +0200

    🐛 showcase form file not found

--- apps/www/src/app/functions/page.tsx
+++ apps/www/src/app/functions/page.tsx
@@ -10,7 +10,42 @@ export default function Page() {
     <>
       <ShowCase
         title="Time"
-        file="/src/client/examples/Time.tsx"
+        // file="/src/client/examples/Time.tsx"
+        code={`import { use } from "react"
+import { z } from "zod"
+import { teampilot } from "~/teampilot"
+
+export const Time = () => {
+  const answer = use(
+    teampilot.functions.fetchData({
+      message: "What time is it in Berlin?",
+      schema: z.object({
+        hours: z.string(),
+        minutes: z.string(),
+        dayOfWeek: z.string(),
+        dateWithoutYear: z.string(),
+        timezone: z.string(),
+      }),
+      cacheTtlSeconds: 60,
+    })
+  )
+  return (
+    <>
+      <div className="flex flex-col items-center gap-2">
+        <div>
+          {answer.dayOfWeek}, {answer.dateWithoutYear}
+        </div>
+        <div className="text-6xl font-bold">
+          <span>{answer.hours}</span>
+          <span className="opacity-40">:</span>
+          <span>{answer.minutes}</span>
+        </div>
+        <div>{answer.timezone}</div>
+      </div>
+    </>
+  )
+}
+`}
         layout="side-by-side"
       >
         <Time />
