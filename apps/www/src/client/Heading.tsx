4cf677ac2b762f5a31df09e4133dd8a260bd0664
Tristan Rechenberger <rechenberger@me.com>
2023-08-03 11:08:46 +0200

    🐛 Heading link

--- apps/www/src/client/Heading.tsx
+++ apps/www/src/client/Heading.tsx
@@ -1,12 +1,11 @@
 import { Link2 } from "lucide-react"
-import Link from "next/link"
 import slugify from "slugify"
 
 export const Heading = ({ children }: { children: string }) => {
   const slug = slugify(children, { lower: true, strict: true })
   return (
     <>
-      <Link href={`#${slug}`}>
+      <a href={`#${slug}`}>
         <h1
           id={slug}
           className="group scroll-m-20 text-4xl font-bold tracking-tight hover:underline"
@@ -14,7 +13,7 @@ export const Heading = ({ children }: { children: string }) => {
           {children}
           <Link2 className="ml-2 hidden group-hover:inline-block" />
         </h1>
-      </Link>
+      </a>
     </>
   )
 }
