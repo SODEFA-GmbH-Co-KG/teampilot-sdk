58e9b6cfea6272f236139cb3fbed41b24146be85
Tristan Rechenberger <rechenberger@me.com>
2023-08-23 19:45:38 +0200

    ✨  widget functions

---
+++ apps/www/src/app/widget/page.tsx
@@ -0,0 +1,58 @@
+"use client"
+
+import { transformZodToJsonSchema } from "@teampilot/sdk"
+import { useEffect, useState } from "react"
+import { z } from "zod"
+import { Heading } from "~/client/Heading"
+
+export default async function Page() {
+  const [color, setColor] = useState("firebrick")
+
+  useEffect(() => {
+    if (typeof window === "undefined") return
+    const widget = (window as any).teampilot
+
+    widget.registerFunction({
+      function: {
+        nameForAI: "changeColor",
+        descriptionForAI: "Change the color of the square using hex codes",
+        inputSchema: transformZodToJsonSchema(
+          z.object({
+            color: z.string(),
+          })
+        ),
+        execute: async ({ input }: any) => {
+          setColor(input.color)
+          return {
+            output: `Changed color to ${input.color}`,
+          }
+        },
+      },
+    })
+
+    return () => {
+      widget.unregisterFunction("changeColor")
+    }
+  })
+
+  return (
+    <>
+      <Heading>Widget</Heading>
+      <div
+        className="flex h-96 w-96 items-center justify-center rounded border text-3xl"
+        style={{
+          backgroundColor: color,
+        }}
+      >
+        {color}
+      </div>
+      <script
+        defer
+        src="http://localhost:3000/widget.js"
+        data-launchpad-slug-id={
+          process.env.NEXT_PUBLIC_LAUNCHPAD_SLUG_ID_LOCAL!
+        }
+      />
+    </>
+  )
+}
