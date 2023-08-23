5b651f16e389c4df72153bfc9cdcf57867871940
Tristan Rechenberger <rechenberger@me.com>
2023-08-23 20:28:35 +0200

    ✨  widget functions

--- apps/www/src/app/widget/page.tsx
+++ apps/www/src/app/widget/page.tsx
@@ -1,47 +1,16 @@
-"use client"
+import { Widget } from "~/client/examples/Widget"
+import { ShowCase } from "~/client/ShowCase"
 
-import { teampilotWidget } from "@teampilot/sdk"
-import { useEffect, useState } from "react"
-import { z } from "zod"
-import { Heading } from "~/client/Heading"
-
-export default async function Page() {
-  const [color, setColor] = useState("#888888")
-
-  useEffect(() => {
-    return teampilotWidget.registerFunction({
-      nameForAI: "changeColor",
-      descriptionForAI: "Change the color of the square using hex codes",
-      inputSchema: z.object({
-        color: z.string(),
-      }),
-      execute: async ({ input }: any) => {
-        setColor(input.color)
-        return {
-          output: `Changed color to ${input.color}`,
-        }
-      },
-    })
-  }, [])
-
+export default function Page() {
   return (
     <>
-      <Heading>Widget</Heading>
-      <div
-        className="flex h-96 w-96 items-center justify-center rounded border text-3xl"
-        style={{
-          backgroundColor: color,
-        }}
+      <ShowCase
+        title="Widget"
+        file="/src/client/examples/Widget.tsx"
+        layout="side-by-side"
       >
-        {color}
-      </div>
-      <script
-        defer
-        src="http://localhost:3000/widget.js"
-        data-launchpad-slug-id={
-          process.env.NEXT_PUBLIC_LAUNCHPAD_SLUG_ID_LOCAL!
-        }
-      />
+        <Widget />
+      </ShowCase>
     </>
   )
 }
