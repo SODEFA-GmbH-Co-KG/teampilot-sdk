7f3066edd8aec31e921f1b3f79dbec26bf150ba9
Pau Kraft <beastmodepau@gmail.com>
2023-10-11 17:49:05 +0200

    ♻️

--- apps/www/src/client/examples/AudioExample.tsx
+++ apps/www/src/client/examples/AudioExample.tsx
@@ -4,10 +4,8 @@ import { env } from "~/env.mjs"
 export const AudioExample = async () => {
   const media = await fetchTeampilotMedia({
     launchpadSlugId: env.LAUNCHPAD_SLUG_ID_WEBSITE_TO_VOICE,
-    message:
-      "Read this Website to me: https://teampilot.ai use the functions please",
-    // cacheTtlSeconds: 60 * 60 * 24, // 1 day
-    cacheTtlSeconds: 0,
+    message: "Read this Website to me: https://teampilot.ai",
+    cacheTtlSeconds: "forever",
   })
 
   return (
