a82109dd5583aae9f5286de457249a3c188c3451
Sean Nicholas Dieterle <dieterle@sodefa.de>
2023-10-13 18:16:05 +0200

    ⏪

--- apps/www/src/app/api/wikipedia/route.ts
+++ apps/www/src/app/api/wikipedia/route.ts
@@ -3,7 +3,6 @@ import { z } from "zod"
 import { fetchWikipediaArticle } from "~/client/examples/wikipedia/fetchWikipedia"
 
 export const { GET, POST } = teampilotFunctionHandler({
-  functionSecret: "fs_b7ce43261f54415c96e6aeedc6f8dcb1",
   functions: [
     {
       nameForAI: "fetchWikipediaArticle",
