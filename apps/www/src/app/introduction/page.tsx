f22b5d6b1124d2b418bc215a04bc463bfee3cd0f
Pau Kraft <beastmodepau@gmail.com>
2023-10-31 15:55:47 +0100

    💬

--- apps/www/src/app/introduction/page.tsx
+++ apps/www/src/app/introduction/page.tsx
@@ -3,23 +3,35 @@ import { DocsLinksGrid, getPageByHref } from "~/client/DocsLink"
 const markdown = `
 # **Welcome to Teampilot**
 
-Teampilot is like ChatGPT, but it comes with more cool stuff like documents, functions, and multiplayer features. 
+Teampilot is a powerful conversational AI platform, akin to ChatGPT, but packed with additional features like document management, built-in functions, and multiplayer capabilities. 
 
-## What can I do with the Teampilot SDK?
-The Teampilot SDK can be broken down into 2 parts:
-**Integrate Teampilot into your product** and **Integrate your product into Teampilot**
+## Exploring the Teampilot SDK
 
-## Integrate Teampilot into your product
-This means using Teampilot AI to programmatically generate text, data or media, with Teampilot, that you can easily integrate into your product.
-For example you could use Teampilot to generate a text for your web page or generate images for your blog posts.
+The Teampilot SDK serves two main functions: 
+1. **[Integrating Teampilot into your product](${
+  getPageByHref("/teampilot-into-your-product")?.href
+})**
+2. **[Integrating your product into Teampilot](${
+  getPageByHref("/your-product-into-teampilot")?.href
+})**
 
-## Integrate your product into Teampilot
-In Teampilot, there are things called [**functions**](${
+Let's delve deeper into each one of these.
+
+## Integrating Teampilot into Your Product
+
+This aspect of the SDK allows you to harness the power of Teampilot's AI to programmatically generate content like text, data, or media. This content can then be seamlessly integrated into your product. For instance, you have the ability to use Teampilot to generate text for your web page or to create bespoke images for your blog posts.
+
+## Integrating Your Product into Teampilot
+
+Within Teampilot, there are features known as [**functions**](${
   getPageByHref("/functions")?.href
-}). Functions are like small programs that Teampilot can use. For example, GPT isn't good at maths, so there's a function called **calculator** that GPT can use to do the math. Or a function that can visit a website.
+}). These functions operate like mini-programs that Teampilot can utilize to extend its capabilities. To illustrate, GPT might not excel at mathematical calculations, so a function named **calculator** is available for GPT to perform such tasks. Another function could be designed to visit specific websites.
 
-With the Teampilot SDK, you can create and connect your own functions to Teampilot. So, for example, you could make a function that can get user data from your database and give it to Teampilot. Then, your Support team could ask Teampilot when a user named "John Doe" last paid. Teampilot would then call your function, your function would find the data from your database and give it back to Teampilot. Teampilot would then answer the question using the info from your function.
-`
+The Teampilot SDK empowers you to create and connect your unique functions to Teampilot. For example, you could develop a function capable of pulling user data from your database and providing it to Teampilot. This means that when anyone within your team asks Teampilot a question related to user data, Teampilot can call the function, the function then fetches the relevant data from your database and feeds it back to Teampilot, which then formulates the answer using the fetched data.
+
+The possibilities are endless with what kind of functions you can develop and integrate. The goal here is to simplify workflows, improve efficiency, and make information retrieval as intuitive as possible. Begin experimenting with the SDK, create [custom functions](${
+  getPageByHref("/custom-functions")?.href
+}), and leverage Teampilot to reinforce your product's capabilities.`
 
 export default function Page() {
   return (
