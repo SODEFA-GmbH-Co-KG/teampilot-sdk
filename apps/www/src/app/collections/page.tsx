f6ea8feba35c4265988a1270b896f8a6ce333472
Tristan Rechenberger <rechenberger@me.com>
2023-11-29 02:20:09 +0100

    ✨ vector collections

--- apps/www/src/app/collections/page.tsx
+++ apps/www/src/app/collections/page.tsx
@@ -1,8 +1,6 @@
 import { Collections } from "~/client/examples/Collections"
 import { ShowCase } from "~/client/ShowCase"
 
-export const dynamic = "force-dynamic" // TODO:  force dynamic because collections are not in teampilot production and we dont have an error while building
-
 export default function Page() {
   return (
     <>
