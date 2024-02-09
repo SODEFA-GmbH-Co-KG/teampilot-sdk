be32983e37ac5e65d1603f880a688f4de67f9bf4
Pau Kraft <beastmodepau@gmail.com>
2024-02-09 11:14:04 +0100

    💬 sdk getting started

--- apps/www/src/app/getting-started/add-ai-to-your-existing-project/page.tsx
+++ apps/www/src/app/getting-started/add-ai-to-your-existing-project/page.tsx
@@ -1,6 +1,49 @@
 import ReactMarkdown from "react-markdown"
 
-const markdown = ``
+const markdown = `
+  ## Get Started
+  
+  The Teampilot Typescript SDK allows you to easily integrate Teampilot in your Typescript Project.
+
+  \`\`\`bash
+    npm install @teampilot/sdk
+  \`\`\`
+
+  ### Basic Usage
+  \`\`\`ts
+  import { fetchTeampilot } from '@teampilot/sdk'
+
+  const response = await fetchTeampilot({
+    launchpadSlugId: process.env.LAUNCHPAD_ID,
+    message: "What is a LLM?",
+    cacheTtlSeconds: 'forever',
+  })
+  \`\`\`
+
+  This will start a new Teampilot chat with the message "What is a LLM?" and cache the response forever.
+  It returns not only the message the LLM sent but also lots of operational data like the id and the url of the chat that it created, or how many TeamTokens it used.
+
+  ### Fetch with in a specific format
+
+  \`\`\`ts
+  import { fetchTeampilot } from '@teampilot/sdk'
+  import { z } from 'zod'
+
+  const response = await fetchTeampilot({
+    launchpadSlugId: process.env.LAUNCHPAD_ID,
+    message: "Give me a list of the top 5 LLMs",
+    cacheTtlSeconds: 'forever',
+    schema: z.object({
+      llms: z.array(z.object({
+        name: z.string(),
+        creator: z.string(),
+      })),
+    })
+  })
+
+  const data = response.message.data // { llms: [{ name: 'LLM1', creator: 'Creator1' }, ...] }
+  \`\`\`
+`
 
 export default function Page() {
   return (
