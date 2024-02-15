98d509dd84720d329a4f56fe8d2951feaccaa5b7
Kevin Graf <graf@freightly.com>
2024-02-15 14:36:20 +0100

    💚

--- apps/www/src/app/examples/Functions.tsx
+++ apps/www/src/app/examples/Functions.tsx
@@ -1,6 +1,8 @@
-import { Form } from "~/client/examples/Form"
+import ReactMarkdown from "react-markdown"
+import { AnchorDiv } from "~/client/AnchorDiv"
 import { Time } from "~/client/examples/Time"
 import { Wikipedia } from "~/client/examples/wikipedia/Wikipedia"
+import { IntersectionChecker } from "~/client/IntersectionChecker"
 import { ShowCase } from "~/client/ShowCase"
 import { getIdForTopic } from "~/utils/navTopics"
 
@@ -28,121 +30,36 @@ export const Functions = async () => {
   })
   return (
     <>
-      {/* <IntersectionChecker topic="/examples#functions" />
+      <IntersectionChecker topic="/examples#functions" />
       <AnchorDiv id={functionsId} />
       <div className="prose dark:prose-invert max-w-[inherit]">
         <ReactMarkdown>{markdown}</ReactMarkdown>
       </div>
       <IntersectionChecker topic={`/examples#${timeId}`} />
-      <AnchorDiv id={timeId} /> */}
+      <AnchorDiv id={timeId} />
       <ShowCase
         title="Time"
         file="/src/client/examples/Time.tsx"
-        //         code={`import { use } from "react"
-        // import { z } from "zod"
-        // import { teampilot } from "~/teampilot"
-
-        // export const Time = () => {
-        //   const answer = use(
-        //     teampilot.functions.fetchData({
-        //       message: "What time is it in Berlin?",
-        //       schema: z.object({
-        //         hours: z.string(),
-        //         minutes: z.string(),
-        //         dayOfWeek: z.string(),
-        //         dateWithoutYear: z.string(),
-        //         timezone: z.string(),
-        //       }),
-        //       cacheTtlSeconds: 60,
-        //     })
-        //   )
-        //   return (
-        //     <>
-        //       <div className="flex flex-col items-center gap-2">
-        //         <div>
-        //           {answer.dayOfWeek}, {answer.dateWithoutYear}
-        //         </div>
-        //         <div className="text-6xl font-bold">
-        //           <span>{answer.hours}</span>
-        //           <span className="opacity-40">:</span>
-        //           <span>{answer.minutes}</span>
-        //         </div>
-        //         <div>{answer.timezone}</div>
-        //       </div>
-        //     </>
-        //   )
-        // }
-        // `}
         layout="side-by-side"
       >
         <Time />
       </ShowCase>
-      {/* <IntersectionChecker topic={`/examples#${formId}`} />
-      <AnchorDiv id={formId} /> */}
-      <ShowCase
+      <IntersectionChecker topic={`/examples#${formId}`} />
+      <AnchorDiv id={formId} />
+      {/* FIXME: The server action seems to break the other components. */}
+      {/* <ShowCase
         title="Form"
         file="/src/client/examples/Form.tsx"
-        //         code={`import { Send } from "lucide-react"
-        // import { Button } from "~/shadcn/components/ui/button"
-        // import { Input } from "~/shadcn/components/ui/input"
-        // import { teampilot } from "~/teampilot"
-
-        // export const Form = () => {
-        //   const submit = async (data: FormData) => {
-        //     "use server"
-        //     const email = data.get("email")
-        //     await teampilot.functions.fetch({
-        //       message: \`Notify me via discord that a new user has signed up with email: \${email}\`,
-        //       cacheTtlSeconds: 0,
-        //     })
-        //   }
-        //   return (
-        //     <>
-        //       <div className="mb-2 text-center font-bold">Register</div>
-        //       <form className="flex flex-row items-center gap-1" action={submit}>
-        //         <Input type="email" placeholder="Your Email" name="email" />
-        //         <Button type="submit" size="sm">
-        //           <Send />
-        //         </Button>
-        //       </form>
-        //     </>
-        //   )
-        // }
-        // `}
         layout="side-by-side"
       >
         <Form />
-      </ShowCase>
-      {/* <IntersectionChecker topic={`/examples#${passingCustomFuctionsId}`} />
-      <AnchorDiv id={passingCustomFuctionsId} /> */}
+      </ShowCase> */}
+      <IntersectionChecker topic={`/examples#${passingCustomFuctionsId}`} />
+      <AnchorDiv id={passingCustomFuctionsId} />
       <ShowCase
         title="Passing custom functions"
         description="You can pass custom functions to the SDK that the AI can call. In this example we pass it a function that can fetch articles from Wikipedia, but you can pass any function you want. For example you could pass a function that fetches data from your database, or a function that mutates things on your side."
         file="/src/client/examples/wikipedia/Wikipedia.tsx"
-        //         code={`import { fetchTeampilotText } from "@teampilot/sdk"
-        // import { z } from "zod"
-        // import { fetchWikipediaArticle } from "./fetchWikipedia"
-
-        // export const Wikipedia = async () => {
-        //   const answer = await fetchTeampilotText({
-        //     message: "How did Luna 25 land on the moon?",
-        //     accessLevel: "LINK_WRITE",
-        //     customFunctions: [
-        //       {
-        //         nameForAI: "fetchWikipediaArticle",
-        //         descriptionForAI: "Fetch a Wikipedia article",
-        //         inputSchema: z.object({
-        //           articleName: z.string(),
-        //         }),
-        //         execute: async ({ input }) => {
-        //           const output = await fetchWikipediaArticle(input.articleName)
-        //           return { output }
-        //         },
-        //       },
-        //     ],
-        //   })
-        //   return answer
-        // }`}
         layout="side-by-side"
       >
         <Wikipedia />
