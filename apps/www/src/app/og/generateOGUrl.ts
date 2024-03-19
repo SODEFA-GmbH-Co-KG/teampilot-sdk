796939db3b8b2c55f1cf81fba95cc0ae4ce83e15
Pau Kraft <beastmodepau@gmail.com>
2024-03-19 22:18:42 +0100

    ♻️

--- apps/www/src/app/og/generateOGUrl.ts
+++ apps/www/src/app/og/generateOGUrl.ts
@@ -1,6 +1,3 @@
 export const generateOgUrl = ({ title }: { title: string }) => {
-  const url = new URL(`${process.env.NEXT_PUBLIC_BASE_URL}/og`)
-  url.searchParams.set("title", title)
-
-  return url.toString()
+  return `${process.env.NEXT_PUBLIC_BASE_URL}/og?title=${title}`
 }
