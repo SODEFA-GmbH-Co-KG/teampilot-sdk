6c351158701c57e5d1782b9378f0fdd659e9a7fc
Pau Kraft <beastmodepau@gmail.com>
2024-03-19 22:14:41 +0100

    ♻️

--- apps/www/src/app/og/route.tsx
+++ apps/www/src/app/og/route.tsx
@@ -2,7 +2,7 @@ import { ImageResponse } from "@vercel/og"
 import { type NextRequest } from "next/server"
 export const runtime = "edge"
 
-export const size = {
+const size = {
   width: 1200,
   height: 630,
 }
