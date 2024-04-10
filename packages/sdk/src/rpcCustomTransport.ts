3479057761ba9699cddc8b5f9c080182e403d2b3
Kevin Graf <graf@freightly.com>
2024-04-10 19:02:46 +0200

    ✨ new transport for cross origin windows

---
+++ packages/sdk/src/rpcCustomTransport.ts
@@ -0,0 +1,39 @@
+import {
+  RPCMessagePortTransportOptions,
+  RPCTransport,
+  rpcTransportMessageIn,
+  rpcTransportMessageOut,
+} from 'rpc-anywhere'
+
+export function createTransportForWindow(
+  window: Window,
+  options: RPCMessagePortTransportOptions = {}
+): RPCTransport {
+  const { transportId, filter, remotePort } = options
+
+  const local = window
+  const remote = (remotePort ?? window) as Window
+
+  let transportHandler: ((event: MessageEvent) => any) | undefined
+  return {
+    send(data) {
+      remote.postMessage(rpcTransportMessageOut(data, { transportId }), '*')
+    },
+    registerHandler(handler) {
+      transportHandler = (event: MessageEvent) => {
+        const message = event.data
+        const [ignore, data] = rpcTransportMessageIn(message, {
+          transportId,
+          filter: () => filter?.(event),
+        })
+        if (ignore) return
+        handler(data)
+      }
+      local.addEventListener('message', transportHandler)
+    },
+    unregisterHandler() {
+      if (transportHandler)
+        local.removeEventListener('message', transportHandler)
+    },
+  }
+}
